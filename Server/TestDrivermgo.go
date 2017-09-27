ceeb6a677aa640eaacc0992ddd7caae4c03d6956
David Silva <davidsilva3290@gmail.com>
2017-09-27 00:44:42 -0500

    Se agrego servidor basico con PAT y Gorilla. Ademas, se agrego el conector mgo, con funciones de insertar y obtener Users

---
+++ Server/TestDrivermgo.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+	// "gopkg.in/mgo.v2/bson"//bson para formato como select where balblala, chekar la documentacion
+)
+////DOCUMENTATION
+//https://godoc.org/gopkg.in/mgo.v2
+//http://www.jancarloviray.com/blog/go-mongodb-simple-example/
+type Users struct {
+	Name string
+}
+
+func main() {
+	session, err := mgo.Dial("mongodb://usuario:contrasena@ds149324.mlab.com:49324/lushflydb")
+	if err != nil {
+			panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// //Pa' insertar
+	// c:= session.DB("lushflydb").C("Users")
+	// err = c.Insert(&Users{Name:"asdsd"})
+	// if err != nil {
+	//  log.Fatal(err)
+	// }
+	
+	//Pa' Obtener
+	var users []Users
+	c:=	session.DB("lushflydb").C("Users")
+	err = c.Find(nil).Sort("-start").All(&users)//es opcional el sort
+	if err != nil {
+		panic(err)
+	}
+	log.Print(users[1].Name)//pera
+}
