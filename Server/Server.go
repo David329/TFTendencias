6a029383fb247b67f722326df07bb5fc64d64aff
David Silva <davidsilva3290@gmail.com>
2017-09-29 02:15:43 -0500

    Se agrego funcionalidades RestFul-WebService con Pat

--- Server/Server.go
+++ Server/Server.go
@@ -1,43 +1,182 @@
-// package main
+////DOCUMENTATION
+//https://godoc.org/gopkg.in/mgo.v2
+//http://www.jancarloviray.com/blog/go-mongodb-simple-example/
+//PREGUNTAR LAS CREDENCIALES DE USUARIO DE BASE DE DATOS
+package main
 
-// import (
-// 	"log"
-// 	"net/http"
+import (
+    "log"
+    "net/http"
+    "encoding/json"
+    "io/ioutil"
 
-// 	"github.com/gorilla/pat"
-// )
+    "github.com/gorilla/pat"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson" //bson para formato como select where balblala, chekar la documentacion
+)
+type Seat struct {
+    UserID string
+    Number string
+    Type string
+}
+type Departure struct {
+    Country string
+    City string
+    TD string //departure, tndria q ser un dateTime
+    TA string //arrival, tndria q ser un dateTime
+}
+type Destination struct {
+    Country string
+    City string
+    TD string //departure, tndria q ser un dateTime
+    TA string //arrival, tndria q ser un dateTime	
+}
+type Flights struct {
+    AirplaneModel string
+    AirplaneNumber string
+    Price float32
+    Depart Departure
+    Destin Destination
+    Seats[] Seat
+}
+//asociar payment, usuario y reserva
+type Payment struct {
+    Card string
+    CardNumber string
+    CSC string
+    Total float32
+    ExpirationDate string //format=(dd/mm/yyyy)
+}
+type Users struct {
+    FirstName string
+    LastName string
+    PassportType string
+    PassportNumber string
+    Email string
+    Password string
+    PersonalCard Payment
+}
 
-// func homeHandler(wr http.ResponseWriter, req *http.Request) {
-// 	wr.WriteHeader(http.StatusOK)
-// 	wr.Write([]byte("Yay! We're home, Jim!"))
-// }
+//FLIGHTS
+func postFlights(wr http.ResponseWriter, req *http.Request) {
 
-// func getAllTheThings(wr http.ResponseWriter, req *http.Request) {
-// 	wr.WriteHeader(http.StatusOK)
-// 	wr.Write([]byte("Look, Jim! Get all the things!"))
-// }
+    //creacion conexion con mlab, con driver mgo
+    session, err:= mgo.Dial("mongodb://userdb:passworddb@ds149324.mlab.com:49324/lushflydb")
+    session.SetMode(mgo.Monotonic, true)
 
-// func putOneThing(wr http.ResponseWriter, req *http.Request) {
-// 	wr.WriteHeader(http.StatusOK)
-// 	wr.Write([]byte("Look, Jim! Put one thing!"))
-// }
+    //obtener el json y lo guardo en body
+    var obj Flights
+    body, err:= ioutil.ReadAll(req.Body)
+    if err != nil {
+        log.Print(err)
+    }
 
-// func deleteOneThing(wr http.ResponseWriter, req *http.Request) {
-// 	wr.WriteHeader(http.StatusOK)
-// 	wr.Write([]byte("Look, Jim! Delete one thing!"))
-// }
+    //parseo de json a flight, nose si parsea mas de 1 objeto..., seguro con un for o algo
+    json.Unmarshal(body, &obj)
 
-// func main() {
-//     router := pat.New()
-// 	// router.Get("/things", getAllTheThings)
+    //inserto en la bd
+    c:= session.DB("lushflydb").C("Flights")
+    err = c.Insert(obj)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-//     // router.Put("/things/{id}", putOneThing)
-//     // router.Delete("/things/{id}", deleteOneThing)
+    //respuesta
+    wr.Header().Set("Content-Type", "application/json")
+    wr.WriteHeader(http.StatusOK)
+    json.NewEncoder(wr).Encode("Obj inserted...")
+}
+//IMPORTANTE ESTE UPDATE NOS PODRIA SERVIR PARA ATRIBUTOS DEL VUELO, NO PARA ACTUALIZAR LOS ASIENTOS OK...
+//PARA ACTUALIZAR LOS ASIENTOS DEBEMOS OBTENER EL USUARIO Y OBTENER EL OBJETO DEL VUELO Y LUEGO AGREGAR Y CONECTAR EN ASIENTOS DE FLIGHT
+func putFlights(wr http.ResponseWriter, req *http.Request) {
 
-// 	router.Get("/", homeHandler)
+    //creacion conexion con mlab, con driver mgo
+    session, err:= mgo.Dial("mongodb://userdb:passworddb@ds149324.mlab.com:49324/lushflydb")
+    session.SetMode(mgo.Monotonic, true)
 
-// 	http.Handle("/", router)
+    //obtener el json y lo guardo en body
+    var obj Flights
+    body, err:= ioutil.ReadAll(req.Body)
+    if err != nil {
+        log.Print(err)
+    }
 
-// 	log.Print("Escuchando en 127.0.0.1:8000...")
-// 	log.Fatal(http.ListenAndServe(":8000", nil))
-// }
+    //parseo de json a flight, lo hacemos para actualizar los asientos
+	json.Unmarshal(body, &obj)
+	
+    //obtener el id desde la url
+	reqId:= req.URL.Query().Get(":id")
+
+    //obtener solo los q tienen ese id
+	c:= session.DB("lushflydb").C("Flights")
+	err = c.UpdateId(bson.ObjectIdHex(reqId),obj)
+
+	////tmb se puede modificar asi, solo atributos
+    // err = c.UpdateId(bson.ObjectIdHex(reqId), bson.M {
+    //         "$set": bson.M {
+    //             "airplanemodel": "uptddsds"
+    //         }
+	// 	})
+
+	//err= c.FindId(bson.ObjectIdHex(reqId)).Sort("-start").One(&obj)//asi obtenemos un objeto
+    if err != nil {
+        panic(err)
+    }
+
+    //respuesta
+    wr.Header().Set("Content-Type", "application/json")
+    wr.WriteHeader(http.StatusOK)
+    json.NewEncoder(wr).Encode("Actualizacion Exitosa")
+}
+
+//USERS
+func getAllUsers(wr http.ResponseWriter, req * http.Request) {
+
+    //creacion conexion con mlab, con driver mgo
+    session, err:= mgo.Dial("mongodb://userdb:passworddb@ds149324.mlab.com:49324/lushflydb")
+    session.SetMode(mgo.Monotonic, true)
+
+    //Pa' Obtener
+    var users[] Users
+    c:= session.DB("lushflydb").C("Users")
+    err = c.Find(nil).Sort("-start").All(&users) //es opcional el sort
+    if err != nil {
+        panic(err)
+    }
+
+    //cerrramos sesion
+    session.Close()
+
+    //parseamos a json
+    data, err:= json.Marshal(users)
+    if err != nil {
+        log.Print(err)
+        return
+    }
+
+    //Formato Json a mostrar
+    wr.Header().Set("Content-Type", "application/json")
+
+    //estado web
+    wr.WriteHeader(http.StatusOK)
+
+    //envio del json a la ruta
+    json.NewEncoder(wr).Encode(string(data))
+}
+
+func main() {
+    //creacion de enrutador pat
+    router:= pat.New()
+
+    //llamada de metodos a enrutar
+    router.Get("/users", getAllUsers)
+    router.Post("/flights", postFlights)
+    router.Put("/flights/{id}", putFlights)
+
+    //activar entutador, probar a;adirlo en otro archivo...
+    http.Handle("/", router)
+
+    //escuchando el servidor
+    log.Print("Escuchando en 127.0.0.1:8000...")
+    log.Fatal(http.ListenAndServe(":8000", nil))
+}
--- Server/TestDrivermgo.go
+++ Server/TestDrivermgo.go
@@ -9,25 +9,56 @@ import (
 ////DOCUMENTATION
 //https://godoc.org/gopkg.in/mgo.v2
 //http://www.jancarloviray.com/blog/go-mongodb-simple-example/
-type Users struct {
-	Name string
+type Payment struct{
+	Card string
+	CardNumber string
+	CSC string
+	Total float32
+	ExpirationDate string //format=(dd/mm/yyyy)
 }
-
+type Users struct {
+	FirstName string
+	LastName string
+	PassportType string
+	PassportNumber string
+	Email string
+	Password string
+	PersonalCard Payment
+}
+//no intentar con la red de la upc, no abrexd
 func main() {
-	session, err := mgo.Dial("mongodb://usuario:contrasena@ds149324.mlab.com:49324/lushflydb")
+	session, err := mgo.Dial("mongodb://userdb:passworddb@ds149324.mlab.com:49324/lushflydb")
 	if err != nil {
 			panic(err)
 	}
 	defer session.Close()
 	session.SetMode(mgo.Monotonic, true)
 
-	// //Pa' insertar
+
+		// //Pa' insertar
+// objAInsertar:= Users{
+// 	FirstName: "ffffcsdsd",
+// 	LastName: "xtuki",
+// 	PassportType: "DNI",
+// 	PassportNumber: "434",
+// 	Email: "david@gmail.com",
+// 	Password: "iksdi",
+// 	PersonalCard: Payment{
+// 		Card: "Visa",
+// 		CardNumber: "2323",
+// 		CSC: "2232323",
+// 		Total: 2332,
+// 		ExpirationDate: "23/21/1923",
+// 	},
+// }
 	// c:= session.DB("lushflydb").C("Users")
-	// err = c.Insert(&Users{Name:"asdsd"})
+	// // err = c.Insert(&Users{Name:"asdsd"})
+	// err=c.Insert(objAInsertar)
 	// if err != nil {
 	//  log.Fatal(err)
 	// }
-	
+	// log.Print(objAInsertar.FirstName);
+
 	//Pa' Obtener
 	var users []Users
 	c:=	session.DB("lushflydb").C("Users")
@@ -35,5 +66,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	log.Print(users[1].Name)//pera
+
+	log.Print(users[0].LastName)//pera
 }
