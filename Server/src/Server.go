4696df911204460be8c9082064e54855ba8e54a5
Kazeia <davidsilva3290@gmail.com>
2017-10-09 00:43:03 -0500

    Se agrego el RestClienthttp, se comento el rabbit por mientras

--- Server/src/Server.go
+++ Server/src/Server.go
@@ -15,7 +15,7 @@ import (
 	Routes "./Routes"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/streadway/amqp"
+	//	"github.com/streadway/amqp"
 )
 
 //Parallelize funcion personalizada para que pueda correr mas de 1 gorutina
@@ -37,69 +37,69 @@ func Parallelize(functions ...func()) {
 func main() {
 
 	listenFirstChannel := func() {
-		conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
-		defer conn.Close()
+		// conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+		// defer conn.Close()
 
-		ch, _ := conn.Channel()
-		defer ch.Close()
+		// ch, _ := conn.Channel()
+		// defer ch.Close()
 
-		q, _ := ch.QueueDeclare(
-			"canal1", // name
-			false,    // durable
-			false,    // delete when unused
-			false,    // exclusive
-			false,    // no-wait
-			nil,      // arguments
-		)
+		// q, _ := ch.QueueDeclare(
+		// 	"canal1", // name
+		// 	false,    // durable
+		// 	false,    // delete when unused
+		// 	false,    // exclusive
+		// 	false,    // no-wait
+		// 	nil,      // arguments
+		// )
 
-		msgs, _ := ch.Consume(
-			q.Name, // queue
-			"",     // consumer
-			true,   // auto-ack
-			false,  // exclusive
-			false,  // no-local
-			false,  // no-wait
-			nil,    // args
-		)
+		// msgs, _ := ch.Consume(
+		// 	q.Name, // queue
+		// 	"",     // consumer
+		// 	true,   // auto-ack
+		// 	false,  // exclusive
+		// 	false,  // no-local
+		// 	false,  // no-wait
+		// 	nil,    // args
+		// )
 
-		log.Printf("Esperando Mensajes en Canal1...")
+		// log.Printf("Esperando Mensajes en Canal1...")
 
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
+		// for d := range msgs {
+		// 	log.Printf("Received a message: %s", d.Body)
+		// }
 	}
 
 	listenSecondChannel := func() {
-		conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
-		defer conn.Close()
+		// conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+		// defer conn.Close()
 
-		ch, _ := conn.Channel()
-		defer ch.Close()
+		// ch, _ := conn.Channel()
+		// defer ch.Close()
 
-		q, _ := ch.QueueDeclare(
-			"canal2", // name
-			false,    // durable
-			false,    // delete when unused
-			false,    // exclusive
-			false,    // no-wait
-			nil,      // arguments
-		)
+		// q, _ := ch.QueueDeclare(
+		// 	"canal2", // name
+		// 	false,    // durable
+		// 	false,    // delete when unused
+		// 	false,    // exclusive
+		// 	false,    // no-wait
+		// 	nil,      // arguments
+		// )
 
-		msgs, _ := ch.Consume(
-			q.Name, // queue
-			"",     // consumer
-			true,   // auto-ack
-			false,  // exclusive
-			false,  // no-local
-			false,  // no-wait
-			nil,    // args
-		)
+		// msgs, _ := ch.Consume(
+		// 	q.Name, // queue
+		// 	"",     // consumer
+		// 	true,   // auto-ack
+		// 	false,  // exclusive
+		// 	false,  // no-local
+		// 	false,  // no-wait
+		// 	nil,    // args
+		// )
 
-		log.Printf("Esperando Mensajes en Canal2...")
+		// log.Printf("Esperando Mensajes en Canal2...")
 
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
+		// for d := range msgs {
+		// 	log.Printf("Received a message: %s", d.Body)
+		// }
 	}
 
 	listenServer := func() {
