55af1d2a34fba86abf7a7e3a9e7d950a0e5b6b04
Kazeia <davidsilva3290@gmail.com>
2017-10-16 17:55:20 -0500

    Se agrego la funcion del Driver de Mongo, con ReplicaSet.

--- Mongo/createReplicaSetMongo.sh
+++ Mongo/createReplicaSetMongo.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#Primero se para el servicio mongod -> service mongod stop
+#IMPORTANTE: Primero se para el servicio mongod -> service mongod stop
+
+#Nos ubicamos en la ruta ./Mongo y ejecutamos los siguientes comandos:
 
 #Creamos la estructura de carpetas para las bases de datos
+cd ./Mongo
 mkdir -p rs1 rs2 rs3
 
 #Iniciamos una BD en el puerto 27017
-sudo mongod --dbpath /root/Escritorio/dev/upc/Tendencias/TF/Mongo/rs1 --replSet lushfly --port 27017
+sudo mongod --dbpath /home/kazeia/dev/upc/Tendencias/TF/Mongo/rs1 --replSet lushfly --port 27017
 
 #Iniciamos una BD en el puerto 27018
-sudo mongod --dbpath /root/Escritorio/dev/upc/Tendencias/TF/Mongo/rs2 --replSet lushfly --port 27018
+sudo mongod --dbpath /home/kazeia/dev/upc/Tendencias/TF/Mongo/rs2 --replSet lushfly --port 27018
 
 #Iniciamos una BD en el puerto 27019
-sudo mongod --dbpath /root/Escritorio/dev/upc/Tendencias/TF/Mongo/rs3 --replSet lushfly --port 27019
+sudo mongod --dbpath /home/kazeia/dev/upc/Tendencias/TF/Mongo/rs3 --replSet lushfly --port 27019
 
 
 ##Para ver los procesos de las BDs: ps -ef | grep mongod
--- Server/src/DB/Access.go
+++ Server/src/DB/Access.go
@@ -9,8 +9,8 @@ import (
 
 //GetDbSession retorna la session de la BD
 func GetDbSession() *mgo.Session {
-	//creacion conexion con mlab, con driver mgo
-	session, err := mgo.Dial("localhost")
+	//creacion conexion localmente con ReplicaSet, 1Primmary, 2 Slaves, con driver mgo
+	session, err := mgo.Dial("localhost:27017,localhost:27018,localhost:27019")
 	if err != nil {
 		log.Print(err)
 	}
